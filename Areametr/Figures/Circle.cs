8da3ffc0401369616e55f4b15f3f1d48cd951de1
Sergey Poleshchuk <poleshchuk.s@tm.biz-apps.ru>
2024-05-30 15:15:09 +0300

    Add project files.

---
+++ Areametr/Figures/Circle.cs
@@ -0,0 +1,21 @@
+﻿using Areametr.Abstraction;
+
+namespace Areametr.Figures
+{
+    public class Circle : IFigure
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(radius);
+
+            this.radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+}
---
+++ Areametr/Figures/Triangle.cs
@@ -0,0 +1,47 @@
+﻿using Areametr.Abstraction;
+
+namespace Areametr.Figures
+{
+    public class Triangle : IFigure
+    {
+        private double sideA, sideB, sideC;
+
+        /// <summary>
+        /// Является ли треугольник прямоугольным
+        /// </summary>
+        public bool IsRightTriangle { get; init; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            Validate(sideA, sideB, sideC);
+
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+
+            IsRightTriangle = IsRight(sideA, sideB, sideC);
+        }
+
+        public double CalculateArea()
+        {
+            var p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+
+        private void Validate(double a, double b, double c)
+        {
+            if (CheckTriangleInequality(a, b, c)
+                && CheckTriangleInequality(b, a, c)
+                && CheckTriangleInequality(c, b, a))
+                return;
+            throw new ArgumentOutOfRangeException("Не соблюдены размеры сторон");
+        }
+
+        private static bool IsRight(double a, double b, double c)
+                => (a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a);
+
+        public static bool CheckTriangleInequality(double a, double b, double c)
+                => a < b + c;
+
+    }
+}
